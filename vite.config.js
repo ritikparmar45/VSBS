b907b6b3567f4eb6dcad03bad0800cb92941b6b0
ritikparmar45 <parmarritikk@gmail.com>
2025-07-18 15:58:41 +0530

    do some changes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Get the backend URL from .env file during production
 const backendURL = process.env.VITE_API_BASE || 'http://localhost:5000';
 
 export default defineConfig({
+  root: 'src', // because index.html is now in /src
   plugins: [react()],
   server: {
     port: 3000,
@@ -15,7 +15,11 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    outDir: '../dist',  // tells Vite to output dist in root
+    emptyOutDir: true,
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-})
+});
